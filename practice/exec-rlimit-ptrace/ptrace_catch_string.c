8c43639f1d67132abfb7f80269db032bbd840b78
Victor Yacovlev <v.yacovlev@gmail.com>
2018-12-06 13:02:14 +0300

    example made more clear

--- practice/exec-rlimit-ptrace/ptrace_catch_string.c
+++ practice/exec-rlimit-ptrace/ptrace_catch_string.c
@@ -16,12 +16,12 @@
 static void
 premoderate_write_syscall(pid_t pid, struct user_regs_struct state)
 {
-    size_t orig_buf = state.rsi;
-    size_t size = state.rdx;
+    size_t orig_buf = state.rsi;   // ecx for i386
+    size_t size = state.rdx;       // rdx for i386
     char *buffer = calloc(size+sizeof(long), sizeof(*buffer));
     int val = 0;
     for (size_t i=0; i<size; ++i) {
-         buffer[i] = ptrace(PTRACE_PEEKDATA, pid, orig_buf+i);
+        buffer[i] = ptrace(PTRACE_PEEKDATA, pid, orig_buf+i, NULL);
     }
     char *bad_word;
     if ( (bad_word=strstr(buffer, "fuck")) ) {
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
     pid_t  pid = fork();
     if (-1==pid) { perror("fork"); exit(1); }
     if (0==pid) {
-        ptrace(PT_TRACE_ME, 0, 0, 0, 0);
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         execvp(argv[1], argv+1);
         perror("exec");
         exit(2);
@@ -50,12 +50,12 @@ int main(int argc, char *argv[])
         struct user_regs_struct state;
         bool stop = false;
         while (!stop) {
-            ptrace(PTRACE_SYSCALL, pid, 0, 0);
+            ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
             waitpid(pid, &wstatus, 0);
             stop = WIFEXITED(wstatus);
             if (WIFSTOPPED(wstatus)) {
                 ptrace(PT_GETREGS, pid, 0, &state);
-                if (__NR_write==state.orig_rax) {
+                if (__NR_write==state.orig_rax) {  // orig_eax for i386
                     premoderate_write_syscall(pid, state);
                 }              
             }
